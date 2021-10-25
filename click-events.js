6217619327e5af4c94794cf4fe8745a249946266
Owen Dwyer <oweyer@CSL14416.local>
2021-10-25 10:15:44 +0100

    Refactor: split CSS+JS into separate files

---
+++ click-events.js
@@ -0,0 +1,43 @@
+$("#reload").click(function() {
+
+    var cypher = $("#cypher").val();
+
+    if (cypher.length > 3) {
+        viz.renderWithCypher(cypher);
+    } else {
+        console.log("reload");
+        viz.reload();
+
+    }
+});
+
+$("#stabilize").click(function() {
+    viz.stabilize();
+})
+
+
+$("#all_patients").click(function() {
+    var query = "MATCH (n:patient) RETURN n"
+    update(query)
+})
+$("#full_graph").click(function() {
+    var query = "MATCH (n)-[r]->(m) RETURN n,r,m"
+    update(query)
+})
+$("#pathways").click(function() {
+    var query = "MATCH (n)-[r:precedes]->(m) RETURN n,r,m"
+    update(query)
+})
+$("#pathways_events").click(function() {
+    var query = "MATCH (e1)-[r1:precedes]->(e2)-[r2]->(o:opcs4) RETURN e1,r1,e2,r2,o"
+    update(query)
+})
+$("#pathways_events_hierarchy").click(function() {
+    var query = "MATCH (e1)-[r1:precedes]->(e2)-[r2*]->(o:opcs4) RETURN e1,r1,e2,r2,o"
+    update(query)
+})
+
+function update(query){
+    $("textarea#cypher").val(query);
+    viz.renderWithCypher(query);
+}
---
+++ vis-window.js
@@ -0,0 +1,142 @@
+function getOPCSLevel(neo4jObject) {
+    title = neo4jObject.properties["name"]
+    console.log(title)
+    console.log(title.length)
+    length = title.length
+
+    if (title=='procedures') {
+        level = 6
+    } else if (length>=5) {
+        level = 5
+    } else {
+        level = 7-length
+    }
+    return level
+}
+
+var viz;
+
+function draw() {
+    var config = {
+        containerId: "viz",
+        neo4j: {
+            serverUrl: "bolt://1069e392.databases.neo4j.io",
+            serverUser: "neo4j",
+            serverPassword: "C315QwpDKsTlYkp1vC78I0kZTvO5x8RuHcHPhyUla7Y",
+            driverConfig: {
+                encrypted: "ENCRYPTION_ON",
+                trust: "TRUST_SYSTEM_CA_SIGNED_CERTIFICATES"
+            }
+        },
+
+        visConfig: {
+            // https://visjs.github.io/vis-network/docs/network/
+            edges: {
+                arrows: {
+                    to:{enabled:true}
+                }
+            },
+            layout: {
+                hierarchical: {
+                    enabled: true,
+                    sortMethod: 'directed',
+                    shakeTowards: 'roots',
+                    edgeMinimization: false,
+                    levelSeparation:300
+                }
+            },
+            nodes : {
+                shape: 'dot',
+            }
+            //hierarchical:true,
+            //hierarchical_sort_method:'directed',
+            //consoleDebug:true
+
+        },
+
+        
+        labels: {
+            //"Character": "name",
+            "event": {
+
+                [NeoVis.NEOVIS_ADVANCED_CONFIG]: {// here you put node properties that aren't mapped directly from the neo4j node
+                    function: {
+                        
+                    },
+                    static: { // everything here will be copied directly to the vis.js's node object
+                        font: {
+                            color: "#000000"
+                        },
+                        level: 2,
+                        color:"#005EB8"
+                    },
+                }
+
+                //"size": "pagerank",
+                //"community": "community"
+                //"sizeCypher": "MATCH (n) WHERE id(n) = {id} MATCH (n)-[r]-() RETURN sum(r.weight) AS c"
+            },
+
+            "patient": {
+                [NeoVis.NEOVIS_ADVANCED_CONFIG]: {// here you put node properties that aren't mapped directly from the neo4j node
+                    static: { // everything here will be copied directly to the vis.js's node object
+                        level:1,
+                        color:'#768692'
+                    },
+                }
+            },
+            "opcs4": {
+                [NeoVis.NEOVIS_ADVANCED_CONFIG]: {// here you put node properties that aren't mapped directly from the neo4j node
+                    static: { // everything here will be copied directly to the vis.js's node object
+                        level:3,
+                        color:'#78BE20'
+                    },
+                    function: {
+                        level: (props) => getOPCSLevel(props)
+
+                    }
+                }
+            },
+            "diagnosis": {
+                [NeoVis.NEOVIS_ADVANCED_CONFIG]: {// here you put node properties that aren't mapped directly from the neo4j node
+                    static: { // everything here will be copied directly to the vis.js's node object
+                        level:3,
+                        color:'#AE2573'
+                    },
+                    function: { // same as label advance function
+                        //label: NeoVis.objectToTitleString // putting caption on the title
+                        //label: (props) => NeoVis.objectToTitleString(props, ["name"])
+                    }
+                }
+            },
+            [NeoVis.NEOVIS_DEFAULT_CONFIG]: {
+                 label: "name",
+                 [NeoVis.NEOVIS_ADVANCED_CONFIG]: {// here you put node properties that aren't mapped directly from the neo4j node
+                    static: { // everything here will be copied directly to the vis.js's node object
+                        level:4
+                    }
+                 }
+
+            }
+        },
+        relationships: {
+            [NeoVis.NEOVIS_DEFAULT_CONFIG]: {
+                 "thickness": "defaultThicknessProperty",
+                 //"caption": true
+            },
+            "precedes": {
+                "thickness": 1,
+                value: "name"
+            }
+        },
+
+
+        initialCypher: "MATCH (n)-[r]->(m) RETURN n,r,m",
+
+    };
+
+    viz = new NeoVis.default(config);
+    viz.render();
+    console.log(viz);
+
+}
